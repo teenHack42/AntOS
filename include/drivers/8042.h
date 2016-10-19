ea6a1c21de24d85d1d986020e33eb6098e32a540
Grant Perry <grant42perry@gmail.com>
2016-10-19 18:24:02 +0000

    inital v2

--- include/drivers/8042.h
+++
@@ -1,7 +0,0 @@
-#ifndef KB8042_H
-#define KB8042_H
-#include <drivers/keyboard.h>
-
-bool init8042();
-
-#endif//KB8042_H
--- include/drivers/keyboard.h
+++
@@ -1,8 +0,0 @@
-#ifndef KEYBOARD_H
-#define KEYBOARD_H
-
-#include <stdbool.h>
-
-bool initKeboard();
-
-#endif//KEYBOARD_H
