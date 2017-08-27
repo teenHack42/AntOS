005bd821f5cec618e49a7b08c994ca29f9484453
teenHack42 <grant42perry@gmail.com>
2017-08-27 13:06:08 +1000

    printf moved to stdio.s memset moved to string.s all as per the spec

--- src/mm.s
+++ src/mm.s
@@ -1,7 +1,6 @@
 [BITS 32]
 
 [GLOBAL init_mm]
-[GLOBAL memset]
 
 [EXTERN put_string]
 [EXTERN to_hex]
@@ -34,23 +33,3 @@ init_mm:
 
 	mov eax,0       ;  normal, no error, return value
 	ret 0
-
-;void *memset(void *str[8], int c[12], size_t n[16])
-memset:
-	push ebp							; set up stack frame
-	mov	ebp, esp
-	sub esp, 0						;no local variables... yet. this is not nescessary but im learning
-
-	mov edi, [ebp+8]			;pointer to the start of the memory we want to set
-	mov al, [ebp+12]			;character
-	mov ecx, [ebp+16]			;length or size
-	cld										;so none of the direcgion flags effect rep(zf)
-	rep stosb							;repeat(mov edi, al; inc edi)
-
-	mov eax, [ebp+12]			;return value(pointer to the memory)
-
-	mov esp, ebp				; takedown stack frame
-  pop ebp							; same as "leave" op
-
-	;mov eax,0       ;  normal, no error, return value
-	ret 0									;tell ret how many local variables we had
--- src/string.s
+++ src/string.s
@@ -1,6 +1,7 @@
 [BITS 32]
 
 [GLOBAL printf]
+[GLOBAL memset]
 
 [EXTERN to_hex]
 [EXTERN put_string]
@@ -9,116 +10,22 @@
 [EXTERN set_cursor]
 [EXTERN cursor_y]
 
+;void *memset(void *str[8], int c[12], size_t n[16])
+memset:
+	push ebp							; set up stack frame
+	mov	ebp, esp
+	sub esp, 0						;no local variables... yet. this is not nescessary but im learning
 
+	mov edi, [ebp+8]			;pointer to the start of the memory we want to set
+	mov al, [ebp+12]			;character
+	mov ecx, [ebp+16]			;length or size
+	cld										;so none of the direcgion flags effect rep(zf)
+	rep stosb							;repeat(mov edi, al; inc edi)
 
-e dd 'END',0
-;int printf(const char *format, ...)
-;[ebp+0x04] => *format
-;[ebp-0x04] => number of operands (...)
-;[ebp-0x05] => index in operand check of postfixes
-printf:
-	push ebp
-	mov ebp, esp
-	sub esp, 5
-	;-----------
-	mov dword [ebp-0x04], 0				;set the number of known operands to 0
-	mov esi, [ebp+0x8]									;set esi to the pointer to the *format string
-	;mov [ebp-0x04], eax									;the count of how many operands
+	mov eax, [ebp+12]			;return value(pointer to the memory)
 
-	.loop_char:
-		mov dl, [esi]
-		and edx, 0x000000FF							;extra because edx may have junk...
+	mov esp, ebp				; takedown stack frame
+  pop ebp							; same as "leave" op
 
-		cmp dl, 0x00
-		je .end
-		cmp dl, '%'
-		je .operand
-		cmp dl, '\'
-		je .escape
-		push edx
-		call put_char
-		inc esi
-		jmp .loop_char
-
-	.operand:
-		inc esi
-		inc dword [ebp-0x04]								;increment the operand index
-		mov dl, [esi]
-		cmp dl, 's'
-		je .op_string
-		cmp dl, 'X'
-		je .op_up_hex
-		cmp dl, 'x'
-		je .op_up_hex
-
-		.operand_end:
-		inc esi
-		jmp .loop_char
-
-		.op_string:
-			mov eax, [ebp-0x04]								;get which operand this is from the variable
-			mov ebx, 0x04											;multiply it by 4 cause each operand is a dword
-			mul ebx
-			add eax, 0x08											;offset by the variables at the start
-			add eax, ebp											;then then the location of the base
-			mov eax, [eax]										;and now we have the pointer to the string
-			call put_string
-			jmp .operand_end
-
-		.op_up_hex:
-			mov eax, [ebp-0x04]								;get which operand this is from the variable
-			mov ebx, 0x04											;multiply it by 4 cause each operand is a dword
-			mul ebx
-			add eax, 0x08											;offset by the variables at the start
-			add eax, ebp											;then then the location of the base
-			mov eax, [eax]										;and now we have the pointer to the number
-			call to_hex
-			call put_string
-			jmp .operand_end
-
-	.escape:
-		inc esi
-		mov dl, [esi]
-		and edx, 0x000000FF							;extra because edx may have junk...
-		cmp dl, 'n'
-		je .escape_nl
-		cmp dl, '\'
-		je .escape_backslash
-		jmp .escape_end
-
-		.escape_nl:
-			call newline
-			jmp .escape_end
-
-		.escape_backslash:
-			call put_char
-			jmp .escape_end
-
-		.escape_end:
-		inc esi
-		jmp .loop_char
-
-	.end:
-	;-----------
-	mov esp, ebp
-  pop ebp
-	mov eax, 0													;return value. TODO The number of bytes written
-  ret 5
-
-operand_postfixes dd 'cdieEfgGosuxXpn',0; all the valid operand postfixes
-
-newline:
-	push ebp
-	mov ebp, esp
-	sub esp, 4
-	push eax
-
-	mov eax, [cursor_y]
-	inc al
-	call set_cursor
-
-	pop eax
-	mov esp, ebp
-  pop ebp
-	mov eax, 0
-	ret
+	;mov eax,0       ;  normal, no error, return value
+	ret 0									;tell ret how many local variables we had
